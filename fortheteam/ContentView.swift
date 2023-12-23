c34e2909269904cbe3a8222e43b75763b166d192
hidirektor <hidirektor@gmail.com>
2023-12-23 20:27:26 +0300

    Initial Commit

---
+++ fortheteam/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  fortheteam
+//
+//  Created by Halil İbrahim Direktör on 23.12.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ fortheteam/fortheteamApp.swift
@@ -0,0 +1,17 @@
+//
+//  fortheteamApp.swift
+//  fortheteam
+//
+//  Created by Halil İbrahim Direktör on 23.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct fortheteamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
